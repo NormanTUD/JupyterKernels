43f20fe260756180749cb4b94b43fc558d93796f
norman koch alpha <norman.koch@tu-dresden.de>
2024-07-30 16:32:30 +0200

    fix

--- build_kernels.sh
+++ build_kernels.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-trap 'calltracer' ERR
+function displaytime {
+        set +x
+        local T=$1
+                local D=$((T/60/60/24))
+                local H=$((T/60/60%24))
+                local M=$((T/60%60))
+                local S=$((T%60))
+                (( $D > 0 )) && printf '%d days ' $D
+                (( $H > 0 )) && printf '%d hours ' $H
+                (( $M > 0 )) && printf '%d minutes ' $M
+                (( $D > 0 || $H > 0 || $M > 0 )) && printf 'and '
+                printf '%d seconds\n' $S
+}
 
 function calltracer {
 	LINE_AND_FUNCTION=$(caller)
@@ -8,6 +20,8 @@ function calltracer {
 	caller
 	echo "Runtime (calltracer): $(displaytime $SECONDS), PID: $$"
 }
+
+trap 'calltracer' ERR
 
 CONFIG_JSON=$(echo '
 		{
